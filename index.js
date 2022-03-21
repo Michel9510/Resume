d24977dab353b313e8b8a2cd841c576eb6b0361e
Michel Lizardo <michel@Michels-MacBook-Pro.local>
2022-03-21 16:54:56 -0400

    resume

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const fs = require('fs')
 
-fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(data)
-        return data
-    }
+fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(data)
+    return data
+  }
 })
